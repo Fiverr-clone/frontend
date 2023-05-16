47ab00700174b15ef837c7bf1c5a12e8f1c16d6d
Amina <aminabakkali2001@gmail.com>
2023-05-17 00:14:57 +0100

    fixed navbarAfter and navbarMenu issues

--- src/components/navbarAfterComponent/navbarAfter.tsx
+++ src/components/navbarAfterComponent/navbarAfter.tsx
@@ -9,56 +9,56 @@ import NavbarMenu from "../navbarMenuComponent/navbarMenu";
 interface NavbarAfterProps {}
 
 const NavbarAfter: FunctionComponent<NavbarAfterProps> = () => {
-  const [subCatId, setSubCatId] = useState<String>("");
+	const [subCatId, setSubCatId] = useState<String>("");
 
-  const handleSubCatId = (value: String) => {
-    setSubCatId(value);
-    // console.log(value);
-  };
+	const handleSubCatId = (value: String) => {
+		setSubCatId(value);
+		// console.log(value);
+	};
 
-  return (
-    <div>
-      <nav className="navbar-after">
-        <ul>
-          <li>
-            <img
-              src={Logo}
-              height={"30px"}
-              width={"auto"}
-              alt="Fiverr Logo"
-              className="navbar-after-logo"
-            />
-          </li>
-          <li>
-            <Searchbar />
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/" className="Business">
-              FIverr Business
-            </Link>
-          </li>
-          <li>
-            <Link to="/">Explore</Link>
-          </li>
+	return (
+		<div>
+			<nav className="navbar-after">
+				<ul>
+					<li>
+						<img
+							src={Logo}
+							height={"30px"}
+							width={"auto"}
+							alt="Fiverr Logo"
+							className="navbar-after-logo"
+						/>
+					</li>
+					<li>
+						<Searchbar />
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<Link to="/" className="Business">
+							FIverr Business
+						</Link>
+					</li>
+					<li>
+						<Link to="/">Explore</Link>
+					</li>
 
-          <li>
-            <Link to="/">Become a Seller</Link>
-          </li>
-          <li>
-            <Link to="/signin">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/join" className="join-link">
-              Join
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <NavbarMenu CategoryId={handleSubCatId} />
-    </div>
-  );
+					<li>
+						<Link to="/">Become a Seller</Link>
+					</li>
+					<li>
+						<Link to="/signin">Sign In</Link>
+					</li>
+					<li>
+						<Link to="/join" className="join-link">
+							Join
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			{/* <NavbarMenu CategoryId={handleSubCatId} /> */}
+		</div>
+	);
 };
 
 export default NavbarAfter;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,38 +5,50 @@ import NavbarAfter from "../../components/navbarAfterComponent/navbarAfter";
 import Featured from "../../components/featured/Featured";
 import Trusted from "../../components/trusted/Trusted";
 import Footer from "../../components/footerComponent/footer";
+import NavbarMenu from "../../components/navbarMenuComponent/navbarMenu";
 
 const HomePage = () => {
-   
+	const [subCatId, setSubCatId] = useState<String>("");
 
-  const [scrolled, setScrolled] = useState(false);
+	const handleSubCatId = (value: String) => {
+		setSubCatId(value);
+		// console.log(value);
+	};
+	const [scrolled, setScrolled] = useState(false);
 
-  const handleScroll = () => {
-    if (window.scrollY > 0) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  };
+	const handleScroll = () => {
+		if (window.scrollY > 0) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <div>
-      {scrolled ? <NavbarAfter /> : <NavbarBefore />}
-      <div className="navbar-menu-container">
-        <Featured />
-        <Trusted />
-        <Footer/>
-        {/* Contenu de votre page */}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			{scrolled ? (
+				<div id="home-navbar" style={{ position: "fixed" }}>
+					<NavbarAfter />
+					<NavbarMenu CategoryId={handleSubCatId} />
+				</div>
+			) : (
+				<NavbarBefore />
+			)}
+			<div className="navbar-menu-container">
+				<Featured />
+				<Trusted />
+				<Footer />
+				{/* Contenu de votre page */}
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
